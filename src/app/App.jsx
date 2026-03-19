e575303f5e932a5d9b823b6d893b8157975b7801
geo-777 <geo.binu.25@gmail.com>
2026-03-19 19:32:34 +0530

    initial commit

---
+++ src/app/App.jsx
@@ -0,0 +1,14 @@
+import Login from "../features/auth/pages/Login";
+//global styles
+import "../styles/global.css";
+import "../styles/theme.css";
+
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default App;
