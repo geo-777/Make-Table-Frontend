a37db43743096f18ad724d05e321581de4539ad4
geo-777 <geo.binu.25@gmail.com>
2026-04-22 23:27:12 +0530

    feat(dash): added a searchable Select.

--- src/features/dashboard/components/timeTables/ClassTimetable.jsx
+++ src/features/dashboard/components/timeTables/ClassTimetable.jsx
@@ -1,8 +1,19 @@
 import styles from "./Timetable.module.css";
+import SearchableSelect from "../../../../shared/components/searchableSelect/SearchableSelect";
 const ClassTimetable = () => {
+  const MOCK_DATA = [
+    { label: "S! CSE", value: 1 },
+    { label: "S2 CSE", value: 1 },
+  ];
   return (
     <div className={styles.main}>
-      <p>Class table </p>
+      <div style={{ maxWidth: "200px" }}>
+        <SearchableSelect
+          options={MOCK_DATA}
+          initialPlaceholder={"Select class"}
+          setValue={() => {}}
+        />
+      </div>
     </div>
   );
 };
