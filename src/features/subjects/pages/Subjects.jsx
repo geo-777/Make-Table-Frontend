3f3f0c9209c2f45b440ecc92113441f63633cc7b
asifali411 <asifalipm411@gmail.com>
2026-05-24 08:24:36 +0530

    feat(subjects): add PageHeader component with dynamic content and styling

--- src/features/subjects/pages/Subjects.jsx
+++ src/features/subjects/pages/Subjects.jsx
@@ -1,10 +1,33 @@
 import Topbar from "../../../shared/components/topbar/Topbar";
 import "../../../styles/appLayout.css";
+import PageHeader from "../../../shared/components/pageHeader/PageHeader";
+import useTimeTableSelect from "../../../shared/zustand/timetableSelectStore";
+import { useState } from "react";
+
 const Subjects = () => {
+
+  const { selectedTimetableData } = useTimeTableSelect();
+  const [activeView, setActiveView] = useState("grid");
+
   return (
     <div className="App">
       <div className="mainPlaceholder">
         <Topbar page={"Subjects"} />
+
+        <PageHeader
+          title={"Subjects"}
+          description={
+            selectedTimetableData?.name ? 
+            `Define subject constraints for ${selectedTimetableData.name}` : 
+            "Select a timetable to define subject constraints."}
+          addButtonText={"Add Subject"}
+          activeView={activeView}
+          onChangeActiveView={(view) => setActiveView(view)}
+          onAdd={() => {}}
+          onBulkImport={() => {}}
+        />
+
+
       </div>
     </div>
   );
---
+++ src/shared/components/pageHeader/PageHeader.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import styles from "./PageHeader.module.css";
+import { Plus, Download, List, Grid2x2 } from "lucide-react";
+
+export default function PageHeader({
+  title,
+  description,
+  addButtonText,
+  activeView,
+  onChangeActiveView,
+  onAdd,
+  onBulkImport,
+}) {
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.headings}>
+        <h4>{title}</h4>
+        <p>{description}</p>
+      </div>
+      <div className={styles.rightPanel}>
+        <div className={styles.secondaryBtnsContainer}>
+          <div className={styles.viewChangerContainer}>
+            <span
+              onClick={() => onChangeActiveView("list")}
+              className={`${styles.viewChangeBtn} ${activeView === "list" ? styles.viewActive : ""} `}
+            >
+              <List size={18} strokeWidth={2} />
+            </span>
+            <span
+              onClick={() => onChangeActiveView("grid")}
+              className={`${styles.viewChangeBtn} ${activeView === "grid" ? styles.viewActive : ""} `}
+            >
+              <Grid2x2 size={18} strokeWidth={2} />
+            </span>
+          </div>
+          <button className={styles.secondaryBtn} onClick={onBulkImport}>
+            {" "}
+            <Download strokeWidth={1.7} size={18} /> <p>Bulk Import</p>{" "}
+          </button>
+        </div>
+
+        <button className={`${styles.primary} ${styles.addBtn}`} onClick={onAdd}>
+          <Plus strokeWidth={2} /> <p>{addButtonText}</p>{" "}
+        </button>
+      </div>
+    </header>
+  );
+}
